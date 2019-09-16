b4caa5b0f2d80c04ea8cf7b26430cfa7e8afc6cb
Andrew Pelykh <andrew.pelykh@gmail.com>
2019-09-16 11:11:55 +0300

    Add range and checkbox to OneDimMassTransfer problem test

--- src/index.js
+++ src/index.js
@@ -4,30 +4,54 @@ import OneDimMassTransfer from "./problems/OneDimMassTransfer";
 const problem = new OneDimMassTransfer();
 const result = problem.solve();
 
-console.log(problem.getXAxis())
-
+const xAxis = problem.getXAxis();
 const graphDiv = document.getElementById('graphDiv');
-let t = 0;
 
-setInterval(() => {
-  var layout = {
-    title: `t = ${t}`,
-    uirevision: 'true',
-    yaxis: {
-      range: [0, 100],
-      autorange: false,
-    },
-  };
+const getLayout = (t) => ({
+  title: `t = ${t}`,
+  uirevision: 'true',
+  yaxis: {
+    range: [0, 100],
+    autorange: false,
+  },
+});
 
-
+const renderPlot = (t) => {
   const data = [{
     mode: 'lines',
     line: {color: "#b55400"},
     y: result[t],
-    x: problem.getXAxis(),
+    x: xAxis,
   }];
 
-  Plotly.react(graphDiv, data, layout);
+  Plotly.react(graphDiv, data, getLayout(t));
+};
 
-  t = t < problem.props.t ? t + 1 : 0;
+let interval = true;
+let t = 0;
+
+setInterval(() => {
+  if (interval) {
+    renderPlot(t);
+
+    tRange.value = t;
+    t = t < problem.props.t ? t + 1 : 0;
+  }
 }, 500);
+
+const tInterval =  document.getElementById('tInterval');
+const tRange = document.getElementById('tRange');
+
+tInterval.checked = interval;
+
+tInterval.addEventListener('input', (e) => {
+  interval = tInterval.checked;
+});
+
+tRange.addEventListener('input', (e) => {
+  interval = false;
+  tInterval.checked = false;
+  t = e.target.value;
+
+  renderPlot(t);
+});
